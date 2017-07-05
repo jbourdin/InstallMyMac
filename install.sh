0267b0ac7395002658bf56f73c4ffdb76e49598b
Julien Bourdin <jbourdin@gmail.com>
2017-07-05 19:57:49 +0200

    Simplification

--- install.sh
+++ install.sh
@@ -6,11 +6,5 @@ git --version;
 ## installation de brew
 command -v brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 
-## ajout du tap pour Cask
-(brew tap | grep caskroom/cask) || brew tap caskroom/cask
-(brew tap | grep Homebrew/bundle) || brew tap Homebrew/bundle
-## Ajout des App du store
-(brew ls | grep mas ) || brew install mas
-
 brew bundle
 
